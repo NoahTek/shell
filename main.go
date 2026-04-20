55898d3c4ad04f8ba00a09abe1fca1afbd6fa49c
Manu <49262925+NoahTek@users.noreply.github.com>
2026-04-20 23:23:27 +0530

    Added Custom Prompt; Rudimentary Command History; fixed cd.

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"os/user"
 	"strings"
 )
+
+var history []string
 
 func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
@@ -15,16 +18,23 @@ func execInput(input string) error {
 		return nil
 	}
 
+	history = append(history, input)
+
 	args := strings.Split(input, " ")
 
 	switch args[0] {
 	case "cd":
 		if len(args) < 2 {
-			return errors.New("path required")
+			return os.Chdir("/home/manu/")
 		}
 		return os.Chdir(args[1])
 	case "exit":
 		os.Exit(0)
+	case "history":
+		for i, cmd := range history {
+			fmt.Printf("%d: %s\n", i+1, cmd)
+		}
+		return nil
 	}
 
 	cmd := exec.Command(args[0], args[1:]...)
@@ -34,10 +44,22 @@ func execInput(input string) error {
 	return cmd.Run()
 }
 
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Println("~ ")
+		path, err := os.Getwd()
+		checkErr(err)
+		u, err := user.Current()
+		checkErr(err)
+		host, err := os.Hostname()
+		checkErr(err)
+		fmt.Printf("[%s@%s %s]> ", u.Username, host, path)
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
