45937effbd140bce53f3c662f3b723c53c395f13
Manu <49262925+NoahTek@users.noreply.github.com>
2026-04-21 20:09:43 +0530

    Works with pipes(|); Interrupt signal(^C) works
    - Added pipes (|); works with multiple of them in the same line.
    - Added interrupt signal.
    - Improved history to work with pipes.
    - Changed hardcoded cd home.

--- main.go
+++ main.go
@@ -3,16 +3,51 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
+	"io"
 	"os"
 	"os/exec"
+	"os/signal"
 	"os/user"
 	"strings"
 )
 
 var history []string
 
-func execInput(input string) error {
+func main() {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+
+	go func() {
+		for {
+			<-sigChan
+			fmt.Println()
+			customPrompt()
+		}
+	}()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		customPrompt()
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println()
+			continue
+		}
+
+		if err = processInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+func customPrompt() {
+	path, _ := os.Getwd()
+	u, _ := user.Current()
+	host, _ := os.Hostname()
+	fmt.Printf("[%s@%s %s]> ", u.Username, host, path)
+}
+
+func processInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 	if input == "" {
 		return nil
@@ -20,54 +55,71 @@ func execInput(input string) error {
 
 	history = append(history, input)
 
-	args := strings.Split(input, " ")
-
-	switch args[0] {
-	case "cd":
-		if len(args) < 2 {
-			return os.Chdir("/home/manu/")
-		}
-		return os.Chdir(args[1])
-	case "exit":
-		os.Exit(0)
-	case "history":
-		for i, cmd := range history {
-			fmt.Printf("%d: %s\n", i+1, cmd)
-		}
-		return nil
-	}
-
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-
-	return cmd.Run()
+	return execInput(input)
 }
 
-func checkErr(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+func execInput(input string) error {
+	commands := strings.Split(input, "|")
+	var pipeIn io.ReadCloser
+	var cmds []*exec.Cmd
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		path, err := os.Getwd()
-		checkErr(err)
-		u, err := user.Current()
-		checkErr(err)
-		host, err := os.Hostname()
-		checkErr(err)
-		fmt.Printf("[%s@%s %s]> ", u.Username, host, path)
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+	for i, cmdStr := range commands {
+		args := strings.Fields(cmdStr)
+		if len(args) == 0 {
 			continue
 		}
 
-		if err = execInput(input); err != nil {
-			fmt.Fprintln(os.Stderr, err)
+		cmd := exec.Command(args[0], args[1:]...)
+		cmd.Stderr = os.Stderr
+
+		switch args[0] {
+		case "cd":
+			if len(args) < 2 {
+				home, _ := os.UserHomeDir()
+				return os.Chdir(home)
+			}
+			return os.Chdir(args[1])
+		case "exit":
+			os.Exit(0)
+		case "history":
+			var sb strings.Builder
+			for j, cmdHist := range history {
+				fmt.Fprintf(&sb, "%d: %s\n", j+1, cmdHist)
+			}
+			if i < len(commands)-1 {
+				pipeIn = io.NopCloser(strings.NewReader(sb.String()))
+			} else {
+				fmt.Print(sb.String())
+			}
+			continue
+		}
+
+		if i > 0 {
+			cmd.Stdin = pipeIn
+		}
+
+		if i < len(commands)-1 {
+			var err error
+			pipeIn, err = cmd.StdoutPipe()
+			if err != nil {
+				return err
+			}
+		} else {
+			cmd.Stdout = os.Stdout
+		}
+
+		if err := cmd.Start(); err != nil {
+			return err
+		}
+
+		cmds = append(cmds, cmd)
+	}
+
+	for _, cmd := range cmds {
+		if err := cmd.Wait(); err != nil {
+			return err
 		}
 	}
+
+	return nil
 }
